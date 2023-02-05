2626375261da321f38fc01e379751cb78da60df4
naufal-yafi <muhammadnaufalyafi00@gmail.com>
2023-02-05 11:19:21 +0700

    update content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,25 @@
-import Navbar from './../components/stateless/Navbar'
-import Card from './../components/statefull/Card'
 import ListName from './../components/stateless/props/ListName'
+import CardGroup from '../components/statefull/CardGroup'
+import React from 'react'
 
-const HomePage = () => {
-    return (
-    <div>
-        <Navbar/>
+class HomePage extends React.Component {
+    render() {
+        return (
+        <div className='mb-6'>
+            <CardGroup/>
 
-        <Card/>
-
-        <div className='px-6 mt-5 mb-4 text-xl font-bold text-slate-200'>Daftar Anggota: </div>
-        
-        <div id="list" className='px-6'>
-            <ListName number="01" name="Sujarwo" location="Pekalongan"/>
-            <ListName number="02" name="Susanti" location="Jakarta"/>
-            <ListName number="03" name="Billy" location="Rowoyoso"/>
-            <ListName number="04" name="Jimmy" location="Yogyakarta"/>
-            <ListName number="05" name="Farhan" location="Tegal"/>
-            <ListName/>
-        </div>
-    </div>)
+            <div className='px-6 mt-5 mb-4 text-xl font-bold text-slate-200'>Daftar Anggota: </div>
+            
+            <div id="list" className='px-6'>
+                <ListName number="01" name="Sujarwo" location="Pekalongan"/>
+                <ListName number="02" name="Susanti" location="Jakarta"/>
+                <ListName number="03" name="Billy" location="Rowoyoso"/>
+                <ListName number="04" name="Jimmy" location="Yogyakarta"/>
+                <ListName number="05" name="Farhan" location="Tegal"/>
+                <ListName/>
+            </div>
+        </div>)
+    }
 }
 
 export default HomePage
